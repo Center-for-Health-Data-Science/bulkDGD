cd4d84d815310e507a296d14afdb207c8cbab7a4
ValeSora <sora.valentina1@gmail.com>
2024-05-12 17:59:21 +0200

    updated package version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ author = \
     "Anders Lykkebo-Valløe, Anders Krogh"
 
 # Set the project's version.
-version = "12.05.2024.c"
+version = "12.05.2024.d"
 
 # Set a brief description of the project.
 description = \
