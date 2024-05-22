5e08f8d1fe8491ac2e37e56cb72726a23a9a4289
ValeSora <sora.valentina1@gmail.com>
2024-05-22 17:46:58 +0200

    package version updated to compy with newer setuptools requirements

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ author = \
     "Anders Lykkebo-Valløe, Anders Krogh"
 
 # Set the project's version.
-version = "12.05.2024.d"
+version = "22.05.2024"
 
 # Set a brief description of the project.
 description = \
