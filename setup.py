6b4feea9ee53309f5cf1b811f488a697c2abf4bb
ValeSora <sora.valentina1@gmail.com>
2024-07-29 17:00:47 +0200

    setup.py updated with newest version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ author = \
     "Anders Lykkebo-Valløe, Andreas Bjerregaard, Anders Krogh"
 
 # Set the project's version.
-version = "1.0.3"
+version = "1.0.4"
 
 # Set a brief description of the project.
 description = \
